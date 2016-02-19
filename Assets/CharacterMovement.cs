41b59785af4348227687f163021ae8ec6f925524
gamesketches <sam.von.ehren@gmail.com>
2016-02-19 17:36:07 -0500

    Put in Lea's texture map and add some basic movement. Looks surprisingly good

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	CharacterController controller;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			float vertical = Input.GetAxis("Vertical");
+			controller.Move(vertical * Camera.main.transform.forward * Time.deltaTime * 10f + Physics.gravity);
+		if(Input.GetAxis("Horizontal") != 0) {
+			transform.Rotate(0f, Input.GetAxis("Horizontal") * Time.deltaTime * 100.0f, 0f);
+		}
+	}
+}
