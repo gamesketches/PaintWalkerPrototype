a6144f67c8591986c84ae367bb9105c23fe103c7
gamesketches <sam.von.ehren@gmail.com>
2016-04-15 12:54:20 -0400

    A little bit more onboarding code

---
+++ Assets/OnBoardingScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnBoardingScript : MonoBehaviour {
+
+	Vector3 targetPosition;
+	Vector3 targetRotation;
+	GameObject playerCamera;
+	bool started;
+	// Use this for initialization
+	void Start () {
+		started = false;
+		GameObject player = GameObject.FindWithTag("Player");
+		playerCamera = player.transform.FindChild("Main Camera").gameObject;
+		targetPosition = gameObject.transform.parent.transform.position;
+		targetRotation = gameObject.transform.parent.transform.rotation.eulerAngles;
+		Debug.Log(targetPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) && !started) {
+			playerCamera.MoveTo(targetPosition, 8f, 0, EaseType.easeInOutQuad);
+			playerCamera.RotateTo(targetRotation, 3f, 6f, EaseType.easeInOutQuad);
+		}
+	}
+
+	void rotation() {
+	}
+}
